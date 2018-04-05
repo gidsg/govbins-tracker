8bac743b27fb495f7d95bef217166bcf158489ae
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-05 15:52:36 +0100

    add path helper

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -24,4 +24,6 @@ end
 render 'index'
 end
 
+
+
 end
---
+++ app/helpers/index_helper.rb
@@ -0,0 +1,9 @@
+module IndexHelper
+  def has_bin?(image_paths_matched_to_name, name)
+    image_paths_matched_to_name.any? { |ip| ip[:name] == name } 
+  end
+
+  def image_path(image_paths_matched_to_name, name)
+    (image_paths_matched_to_name.find { |ip| ip[:name] == name })&.dig(:path)
+  end
+end
