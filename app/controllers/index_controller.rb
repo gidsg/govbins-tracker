12077cd24f5508730cfdddeb2bfbbbdb24c0825a
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-05 11:49:18 +0100

    add matcher

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,11 +3,16 @@ require 'octokit'
 class IndexController < ActionController::Base
 
 def show
-client = Octokit::Client.new(
+Octokit::Client.new(
 client_id:  ENV['GITHUB_CLIENT_ID'],
 client_secret: ENV['GITHUB_CLIENT_SECRET'])
-user = Octokit.user 'gidsg'
-puts user.name
+contents = Octokit.contents('HarryTrimble/govbins.uk', path: 'images')
+image_paths = contents.select{ |e| e.download_url.to_s.end_with? '.jpg' }.map(&:download_url)
+@image_path_matched_to_name = image_paths.map{ |ip|
+   {name: ip.tr('-', ' ').split('/images/')[1].split('.')[0], path: ip} 
+  }
+
+puts(@image_path_matched_to_name)
 registers_client = RegistersClient::RegisterClientManager.new
 local_authorities = ['local-authority-eng','local-authority-nir', 'local-authority-sct', 'principal-local-authority']
 local_authorities.each do |local_authority|
