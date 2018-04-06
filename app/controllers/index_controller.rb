8edc21e18b0a2451233ad819a325d0ef8098d933
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-06 17:16:20 +0100

    link to register root

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -17,11 +17,9 @@ class IndexController < ApplicationController
     registers_client = RegistersClient::RegisterClientManager.new
     local_authorities = ['local-authority-eng', 'local-authority-nir', 'local-authority-sct', 'principal-local-authority']
     local_authorities.each do |local_authority|
-      self.instance_variable_set("@#{local_authority.underscore}".to_sym, registers_client.get_register(local_authority, 'beta'))
+      instance_variable_set("@#{local_authority.underscore}".to_sym, registers_client.get_register(local_authority, 'beta'))
     end
     @local_authority_registers = [@local_authority_eng, @local_authority_nir, @local_authority_sct, @principal_local_authority]
-
-
     render :show
   end
 end
