61d5ef1ca6669256033932896f059a3967b495b0
Gideon Goldberg <gidsgoldberg@gmail.com>
2018-04-06 13:43:19 +0100

    lint;

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,28 +1,25 @@
-require 'register_client_manager'  
+require 'register_client_manager'
 require 'octokit'
 class IndexController < ActionController::Base
+  def show
+    Octokit::Client.new(
+      client_id:  ENV['GITHUB_CLIENT_ID'],
+      client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+    contents = Octokit.contents('HarryTrimble/govbins.uk', path: 'images')
+    image_paths = contents.select { |e| e.download_url.to_s.end_with? '.jpg' }.map(&:download_url)
+    @image_path_matched_to_name = image_paths.map { |ip|
+      { name: ip.tr('-', ' ').split('/images/')[1].split('.')[0], path: ip }
+    }
 
-def show
-Octokit::Client.new(
-client_id:  ENV['GITHUB_CLIENT_ID'],
-client_secret: ENV['GITHUB_CLIENT_SECRET'])
-contents = Octokit.contents('HarryTrimble/govbins.uk', path: 'images')
-image_paths = contents.select{ |e| e.download_url.to_s.end_with? '.jpg' }.map(&:download_url)
-@image_path_matched_to_name = image_paths.map{ |ip|
-   {name: ip.tr('-', ' ').split('/images/')[1].split('.')[0], path: ip} 
-  }
-
-registers_client = RegistersClient::RegisterClientManager.new
-local_authorities = ['local-authority-eng','local-authority-nir', 'local-authority-sct', 'principal-local-authority']
-local_authorities.each do |local_authority|
-  self.instance_variable_set("@#{local_authority.underscore}".to_sym, registers_client.get_register(local_authority, 'beta'))
-end
-@local_authority_registers = [@local_authority_eng, @local_authority_nir, @local_authority_sct, @principal_local_authority]
-
-
-render 'index'
-end
+    registers_client = RegistersClient::RegisterClientManager.new
+    local_authorities = ['local-authority-eng', 'local-authority-nir', 'local-authority-sct', 'principal-local-authority']
+    local_authorities.each do |local_authority|
+      self.instance_variable_set("@#{local_authority.underscore}".to_sym, registers_client.get_register(local_authority, 'beta'))
+    end
+    @local_authority_registers = [@local_authority_eng, @local_authority_nir, @local_authority_sct, @principal_local_authority]
 
 
-
+    render 'index'
+  end
 end
--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,9 +1,15 @@
 module IndexHelper
   def has_bin?(image_paths_matched_to_name, name)
-    image_paths_matched_to_name.any? { |ip| ip[:name] == name } 
+    image_paths_matched_to_name.any? { |ip| ip[:name] == name }
   end
 
   def image_path(image_paths_matched_to_name, name)
     image_paths_matched_to_name.find { |ip| ip[:name] == name }&.dig(:path)
+  end
+
+  def register_bin_stats(register, image_paths_matched_to_name)
+    has_bin_count = register.get_records.select { |r| has_bin?(image_paths_matched_to_name, r.item.value['name']) }.length
+    records_count = register.get_records.count
+    { has_bin_count: has_bin_count, register_count: records_count, percentage_complete: "#{(has_bin_count.to_f / records_count.to_f * 100).round}%" }
   end
 end
